5f620f9a9f9e90fb84873e9fd6ea669c2c307bb8
Parwat Kunwar <parwatkunwar08@gmail.com>
2018-01-28 16:06:19 +0545

    Momocha client app basic setup

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+module.exports = {
+  entry: [
+    path.resolve(__dirname, './src')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+
+  //...
+  devServer: {
+    port: 3000,
+    historyApiFallback: true
+  },
+  plugins: [],
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader', 'eslint-loader'] },
+    ]
+  }
+};
